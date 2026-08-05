20be5d4063f9e00460f89793a08023e2ed2bd215
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-05 23:39:29 +0200

    make Lua pixel generator tests resolution-aware

--- src/test/kotlin/de/teutonstudio/ccaeroworks/client/guide/LuaPixelCodeGeneratorTest.kt
+++ src/test/kotlin/de/teutonstudio/ccaeroworks/client/guide/LuaPixelCodeGeneratorTest.kt
@@ -8,23 +8,31 @@ import org.junit.jupiter.api.Test
 
 class LuaPixelCodeGeneratorTest {
     @Test
-    fun `renders a complete five row Lua call`() {
-        var pixels = DeskDisplayPixels.blank(DeskDisplayType.TWO_DIGIT)
-        pixels = pixels.withPixel(0, 0, true).withPixel(6, 4, true)
+    fun `renders a complete configured raster Lua call`() {
+        val type = DeskDisplayType.TWO_DIGIT
+        var pixels = DeskDisplayPixels.blank(type)
+        pixels = pixels
+            .withPixel(0, 0, true)
+            .withPixel(pixels.width - 1, pixels.height - 1, true)
 
         val code = LuaPixelCodeGenerator.fullRaster("left", pixels)
+        val firstRow = "1" + "0".repeat(pixels.width - 1)
+        val lastRow = "0".repeat(pixels.width - 1) + "1"
 
         assertTrue(code.startsWith("desk.setDisplayPixels(\n  \"left\",\n  {"))
-        assertTrue(code.contains("    \"1000000\","))
-        assertTrue(code.contains("    \"0000001\""))
-        assertEquals(10, code.lines().size)
+        assertTrue(code.contains("    \"$firstRow\","))
+        assertTrue(code.contains("    \"$lastRow\""))
+        assertEquals(pixels.height + 5, code.lines().size)
     }
 
     @Test
     fun `converts internal coordinates to Lua coordinates`() {
+        val type = DeskDisplayType.THREE_DIGIT
+        val coordinate = PixelCoordinate(type.pixelWidth - 1, type.pixelHeight - 1)
+
         assertEquals(
-            "desk.setDisplayPixel(\"big\", 11, 5, true)",
-            LuaPixelCodeGenerator.singlePixel("big", PixelCoordinate(10, 4), true)
+            "desk.setDisplayPixel(\"big\", ${type.pixelWidth}, ${type.pixelHeight}, true)",
+            LuaPixelCodeGenerator.singlePixel("big", coordinate, true)
         )
     }
 }
