9bcb2a967abc7577dde7fe812a8cd8a7d56f3471
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-05 18:29:36 +0200

    feat: add pixel editor state

---
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/client/guide/PixelEditorState.kt
@@ -0,0 +1,85 @@
+package de.teutonstudio.ccaeroworks.client.guide
+
+import de.teutonstudio.ccaeroworks.display.DeskDisplayPixels
+import de.teutonstudio.ccaeroworks.display.DeskDisplayType
+
+data class PixelCoordinate(val x: Int, val y: Int)
+
+class PixelEditorState {
+    var displayType: DeskDisplayType = DeskDisplayType.TWO_DIGIT
+        private set
+
+    var socketName: String = "left"
+        private set
+
+    var selectedPixelValue: Boolean = true
+        private set
+
+    var lastEditedPixel: PixelCoordinate? = null
+        private set
+
+    private val pixelsByType: MutableMap<DeskDisplayType, DeskDisplayPixels> =
+        DeskDisplayType.entries.associateWith(DeskDisplayPixels::blank).toMutableMap()
+
+    val pixels: DeskDisplayPixels
+        get() = pixelsByType.getValue(displayType)
+
+    val width: Int
+        get() = pixels.width
+
+    val height: Int
+        get() = pixels.height
+
+    fun selectDisplayType(type: DeskDisplayType) {
+        displayType = type
+        if (type == DeskDisplayType.THREE_DIGIT) socketName = "big"
+        lastEditedPixel = null
+    }
+
+    fun availableSockets(): List<String> = when (displayType) {
+        DeskDisplayType.TWO_DIGIT -> SMALL_DISPLAY_SOCKETS
+        DeskDisplayType.THREE_DIGIT -> LARGE_DISPLAY_SOCKETS
+    }
+
+    fun selectSocket(socket: String) {
+        require(socket in availableSockets()) { "Socket '$socket' is not valid for $displayType" }
+        socketName = socket
+    }
+
+    fun selectPixelValue(enabled: Boolean) {
+        selectedPixelValue = enabled
+    }
+
+    fun setPixel(x: Int, y: Int, enabled: Boolean = selectedPixelValue) {
+        pixelsByType[displayType] = pixels.withPixel(x, y, enabled)
+        lastEditedPixel = PixelCoordinate(x, y)
+    }
+
+    fun clear() {
+        pixelsByType[displayType] = DeskDisplayPixels.blank(displayType)
+        lastEditedPixel = null
+    }
+
+    fun fill() {
+        pixelsByType[displayType] = DeskDisplayPixels.fromRows(
+            displayType,
+            List(DeskDisplayPixels.HEIGHT) { "1".repeat(DeskDisplayPixels.pixelWidth(displayType)) }
+        )
+        lastEditedPixel = null
+    }
+
+    fun invert() {
+        pixelsByType[displayType] = DeskDisplayPixels.fromRows(
+            displayType,
+            pixels.rows().map { row ->
+                row.map { bit -> if (bit == '1') '0' else '1' }.joinToString("")
+            }
+        )
+        lastEditedPixel = null
+    }
+
+    private companion object {
+        val SMALL_DISPLAY_SOCKETS: List<String> = listOf("left", "right", "big")
+        val LARGE_DISPLAY_SOCKETS: List<String> = listOf("big")
+    }
+}
