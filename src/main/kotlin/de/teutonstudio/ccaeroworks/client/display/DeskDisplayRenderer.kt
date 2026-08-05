a9b958e220783b1c4c45c9756e0197ff7611d519
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-05 23:34:32 +0200

    render configured display heights

--- src/main/kotlin/de/teutonstudio/ccaeroworks/client/display/DeskDisplayRenderer.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/client/display/DeskDisplayRenderer.kt
@@ -24,7 +24,15 @@ object DeskDisplayRenderer {
             val elements = if (display.pixels != null) {
                 buildList {
                     for (y in 0 until display.pixels.height) for (x in 0 until display.pixels.width) {
-                        if (display.pixels.get(x, y)) add(Triple(DeskDisplayModels.PIXEL, pixelOffsetX(display.pixels.width, x), pixelOffsetZ(y)))
+                        if (display.pixels.get(x, y)) {
+                            add(
+                                Triple(
+                                    DeskDisplayModels.PIXEL,
+                                    pixelOffsetX(display.pixels.width, x),
+                                    pixelOffsetZ(display.pixels.height, y)
+                                )
+                            )
+                        }
                     }
                 }
             } else {
@@ -37,15 +45,15 @@ object DeskDisplayRenderer {
                 }
             }
             elements.forEach { (model, x, z) ->
-                    val rendered: SuperByteBuffer = CachedBuffers.partial(model, desk.blockState)
-                        .translate(0.5, 0.5, 0.5)
-                        .rotate(rotation)
-                        .translate(socket.offset().x - 0.5, socket.offset().y - 0.5, socket.offset().z - 0.5)
-                        .rotate(socket.orientation())
-                        .translate(-0.5, 0.0, -0.5)
-                        .translate(x, 0.0, z)
-                    rendered.light<SuperByteBuffer>(light)
-                    rendered.renderInto(poseStack, consumer)
+                val rendered: SuperByteBuffer = CachedBuffers.partial(model, desk.blockState)
+                    .translate(0.5, 0.5, 0.5)
+                    .rotate(rotation)
+                    .translate(socket.offset().x - 0.5, socket.offset().y - 0.5, socket.offset().z - 0.5)
+                    .rotate(socket.orientation())
+                    .translate(-0.5, 0.0, -0.5)
+                    .translate(x, 0.0, z)
+                rendered.light<SuperByteBuffer>(light)
+                rendered.renderInto(poseStack, consumer)
             }
         }
     }
@@ -57,5 +65,5 @@ object DeskDisplayRenderer {
     fun pixelOffsetX(width: Int, x: Int): Double = -(width - 1) * PIXEL_X_SPACING / 2.0 + x * PIXEL_X_SPACING
 
     @JvmStatic
-    fun pixelOffsetZ(y: Int): Double = (de.teutonstudio.ccaeroworks.display.DeskDisplayPixels.HEIGHT - 1) * PIXEL_Z_SPACING / 2.0 - y * PIXEL_Z_SPACING
+    fun pixelOffsetZ(height: Int, y: Int): Double = (height - 1) * PIXEL_Z_SPACING / 2.0 - y * PIXEL_Z_SPACING
 }
