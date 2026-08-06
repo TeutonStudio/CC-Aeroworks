d006fc0e482169d068352731716ae94b3eff8241
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-06 16:49:18 +0200

    keep control desk events local to each adapter

--- src/main/kotlin/de/teutonstudio/ccaeroworks/compat/computercraft/ControlDeskPeripheralState.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/compat/computercraft/ControlDeskPeripheralState.kt
@@ -23,79 +23,26 @@ object ControlDeskPeripheralState {
                 return@removeIf true
             }
 
-            publishAttachedDeskEvents(peripheral)
-            publishMultiblockEvents(peripheral)
-            false
-        }
-    }
-
-    private fun publishAttachedDeskEvents(peripheral: ControlDeskPeripheral) {
-        val current = peripheral.snapshotInputs()
-        val previous = peripheral.lastInputs
-        peripheral.lastInputs = current
-
-        if (previous == null) return
-        InputSnapshotDiff.changed(previous, current).forEach { change ->
-            peripheral.computers.forEach { computer ->
-                computer.queueEvent(
-                    CCAeroworks.INPUT_EVENT,
-                    *DeskInputEventArguments.create(
-                        computer.attachmentName,
-                        change.socket,
-                        change.moduleId,
-                        change.value,
-                        change.channel
-                    )
-                )
-            }
-        }
-    }
-
-    private fun publishMultiblockEvents(peripheral: ControlDeskPeripheral) {
-        val current = peripheral.snapshotNetwork() ?: return
-        val previous = peripheral.lastNetwork
-        peripheral.lastNetwork = current
-
-        if (previous == null) return
-
-        if (previous.signature != current.signature) {
-            peripheral.computers.forEach { computer ->
-                computer.queueEvent(
-                    CCAeroworks.MULTIBLOCK_CHANGED_EVENT,
-                    computer.attachmentName,
-                    current.state.name.lowercase(),
-                    current.memberCount,
-                    current.revision
-                )
-            }
-        }
-
-        (previous.desks.keys + current.desks.keys)
-            .toSortedSet()
-            .forEach { deskId ->
-                val oldDesk = previous.desks[deskId]
-                val newDesk = current.desks[deskId]
-                val deskIndex = newDesk?.index ?: oldDesk?.index ?: return@forEach
-                InputSnapshotDiff.changed(
-                    oldDesk?.inputs.orEmpty(),
-                    newDesk?.inputs.orEmpty()
-                ).forEach { change ->
+            val current = peripheral.snapshotInputs()
+            val previous = peripheral.lastInputs
+            peripheral.lastInputs = current
+            if (previous != null) {
+                InputSnapshotDiff.changed(previous, current).forEach { change ->
                     peripheral.computers.forEach { computer ->
                         computer.queueEvent(
-                            CCAeroworks.MULTIBLOCK_INPUT_EVENT,
-                            computer.attachmentName,
-                            deskId,
-                            deskIndex,
-                            change.socket,
-                            change.moduleId,
-                            change.value,
-                            change.channel,
-                            de.teutonstudio.ccaeroworks.compat.aeroworks.DeskSockets.name(
-                                change.socket
+                            CCAeroworks.INPUT_EVENT,
+                            *DeskInputEventArguments.create(
+                                computer.attachmentName,
+                                change.socket,
+                                change.moduleId,
+                                change.value,
+                                change.channel
                             )
                         )
                     }
                 }
             }
+            false
+        }
     }
 }
