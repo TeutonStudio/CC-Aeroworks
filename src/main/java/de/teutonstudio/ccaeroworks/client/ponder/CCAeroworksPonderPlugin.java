508a85212245b97738da10294e9dda211e00bf65
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-06 11:54:03 +0200

    register display ponder scene

--- src/main/java/de/teutonstudio/ccaeroworks/client/ponder/CCAeroworksPonderPlugin.java
+++ src/main/java/de/teutonstudio/ccaeroworks/client/ponder/CCAeroworksPonderPlugin.java
@@ -10,6 +10,10 @@ public class CCAeroworksPonderPlugin implements PonderPlugin {
         ResourceLocation.fromNamespaceAndPath(CCAeroworks.MOD_ID, "computer_control_desk");
     private static final ResourceLocation ADVANCED_COMPUTER_CONTROL_DESK =
         ResourceLocation.fromNamespaceAndPath(CCAeroworks.MOD_ID, "advanced_computer_control_desk");
+    private static final ResourceLocation TWO_DIGIT_DISPLAY =
+        ResourceLocation.fromNamespaceAndPath(CCAeroworks.MOD_ID, "two_digit_display");
+    private static final ResourceLocation THREE_DIGIT_DISPLAY =
+        ResourceLocation.fromNamespaceAndPath(CCAeroworks.MOD_ID, "three_digit_display");
 
     @Override
     public String getModId() {
@@ -20,5 +24,7 @@ public class CCAeroworksPonderPlugin implements PonderPlugin {
     public void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
         helper.forComponents(COMPUTER_CONTROL_DESK, ADVANCED_COMPUTER_CONTROL_DESK)
             .addStoryBoard("computer_control_desk", ComputerControlDeskScenes::overview);
+        helper.forComponents(TWO_DIGIT_DISPLAY, THREE_DIGIT_DISPLAY)
+            .addStoryBoard("computer_control_desk", DisplayModuleScenes::overview);
     }
 }
