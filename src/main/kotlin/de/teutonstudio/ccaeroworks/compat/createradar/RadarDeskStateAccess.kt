5a5f36b31c8e9cdb74b5b8067bd5f6edea14eda1
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 00:20:46 +0200

    Remove pixel raster state from radar desks

--- src/main/kotlin/de/teutonstudio/ccaeroworks/compat/createradar/RadarDeskStateAccess.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/compat/createradar/RadarDeskStateAccess.kt
@@ -1,21 +1,9 @@
 package de.teutonstudio.ccaeroworks.compat.createradar
 
-import de.teutonstudio.ccaeroworks.display.DeskDisplayPixels
 import de.teutonstudio.ccaeroworks.display.RadarDisplaySnapshot
-import de.teutonstudio.ccaeroworks.display.RadarDisplayType
-
-data class RadarRasterCache(
-    val snapshot: RadarDisplaySnapshot?,
-    val width: Int,
-    val height: Int,
-    val fresh: Boolean,
-    val pixels: DeskDisplayPixels
-)
 
 interface RadarDeskStateAccess {
     fun ccaeroworks_getRadarSnapshot(): RadarDisplaySnapshot?
 
     fun ccaeroworks_setRadarSnapshot(snapshot: RadarDisplaySnapshot?)
-
-    fun ccaeroworks_getRadarPixels(type: RadarDisplayType, gameTime: Long): DeskDisplayPixels
 }
