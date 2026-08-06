62c427f3d441bfed2fa24d6ac7be734c381565f2
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 01:28:43 +0200

    Carry desk orientation into radar surface projection

--- src/main/kotlin/de/teutonstudio/ccaeroworks/display/RadarSurfaceState.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/display/RadarSurfaceState.kt
@@ -1,7 +1,10 @@
 package de.teutonstudio.ccaeroworks.display
 
+import net.minecraft.core.Direction
+
 data class RadarSurfaceState(
     val socket: Int,
     val type: RadarDisplayType,
-    val snapshot: RadarDisplaySnapshot?
+    val snapshot: RadarDisplaySnapshot?,
+    val facing: Direction
 )
