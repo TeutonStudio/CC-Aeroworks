c9561737cd65986c43a68c1ed886a7fde84278a6
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-06 17:11:15 +0200

    update input monitor for desk network handles

--- examples/cc/input-monitor.lua
+++ examples/cc/input-monitor.lua
@@ -1,28 +1,36 @@
--- Prints all CC-Aeroworks control desk input events.
--- Run on a directly attached computer or one connected through a wired modem.
+-- Prints current input values for every Desk adapter in the multiblock.
+-- Run on the embedded Computer Control Desk.
 
-local PERIPHERAL_TYPE = "cc_aeroworks_control_desk"
-local EVENT_NAME = "cc_aeroworks_desk_input"
+local desks = peripherals.find("ControlDesk")
+assert(next(desks), "No ControlDesk adapters in the current network")
 
-local desk = peripheral.find(PERIPHERAL_TYPE)
-assert(desk, "No CC-Aeroworks control desk peripheral found")
-
-print("Connected sockets:")
-for _, socket in ipairs(desk.getSockets()) do
-  print(("  %s = %d"):format(socket.name, socket.index))
-end
-
-print("Waiting for " .. EVENT_NAME .. " events. Press Ctrl+T to stop.")
+print("Monitoring all desk inputs. Press Ctrl+T to stop.")
 while true do
-  local _, peripheralName, socket, moduleId, value, channel, socketName =
-    os.pullEvent(EVENT_NAME)
+  term.clear()
+  term.setCursorPos(1, 1)
 
-  print(("%s %s[%d] %s/%s = %s"):format(
-    peripheralName,
-    socketName,
-    socket,
-    moduleId,
-    channel,
-    textutils.serialize(value, { compact = true })
+  local network = peripherals.getNetwork()
+  print(("Network: %s, desks=%d, peripherals=%d"):format(
+    network.state,
+    network.deskCount,
+    network.peripheralCount
   ))
+
+  for address, desk in pairs(desks) do
+    print("\n" .. address)
+    local inputs = desk.getInputs()
+    if next(inputs) == nil then
+      print("  no input modules")
+    else
+      for socket, value in pairs(inputs) do
+        print(("  socket %d = %s"):format(
+          socket,
+          textutils.serialize(value, { compact = true })
+        ))
+      end
+    end
+  end
+
+  sleep(0.25)
+  desks = peripherals.find("ControlDesk")
 end
