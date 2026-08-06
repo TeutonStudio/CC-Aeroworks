fcd3d0f6e1150d250df388b4d2f91f9ac05a4b86
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-06 23:32:27 +0200

    Remove persisted radar controller links

--- src/main/kotlin/de/teutonstudio/ccaeroworks/compat/createradar/RadarDeskStateAccess.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/compat/createradar/RadarDeskStateAccess.kt
@@ -3,27 +3,6 @@ package de.teutonstudio.ccaeroworks.compat.createradar
 import de.teutonstudio.ccaeroworks.display.DeskDisplayPixels
 import de.teutonstudio.ccaeroworks.display.RadarDisplaySnapshot
 import de.teutonstudio.ccaeroworks.display.RadarDisplayType
-import net.minecraft.core.BlockPos
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.Tag
-
-data class RadarControllerLink(
-    val position: BlockPos,
-    val dimension: String
-) {
-    fun toTag(): CompoundTag = CompoundTag().apply {
-        putLong("position", this@RadarControllerLink.position.asLong())
-        putString("dimension", this@RadarControllerLink.dimension)
-    }
-
-    companion object {
-        fun fromTag(tag: CompoundTag): RadarControllerLink? {
-            if (!tag.contains("position", Tag.TAG_LONG.toInt())) return null
-            val dimension = tag.getString("dimension").takeIf(String::isNotEmpty) ?: return null
-            return RadarControllerLink(BlockPos.of(tag.getLong("position")), dimension)
-        }
-    }
-}
 
 data class RadarRasterCache(
     val snapshot: RadarDisplaySnapshot?,
@@ -34,10 +13,6 @@ data class RadarRasterCache(
 )
 
 interface RadarDeskStateAccess {
-    fun ccaeroworks_getRadarControllerLink(): RadarControllerLink?
-
-    fun ccaeroworks_setRadarControllerLink(link: RadarControllerLink?)
-
     fun ccaeroworks_getRadarSnapshot(): RadarDisplaySnapshot?
 
     fun ccaeroworks_setRadarSnapshot(snapshot: RadarDisplaySnapshot?)
