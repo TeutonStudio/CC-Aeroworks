2ec187ff4c110eb5e3c1770eecc673f2a63ee535
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 13:18:50 +0200

    Trace radar client bootstrap and overlay registration

--- src/main/kotlin/de/teutonstudio/ccaeroworks/client/CCAeroworksClient.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/client/CCAeroworksClient.kt
@@ -7,6 +7,7 @@ import de.teutonstudio.ccaeroworks.client.creative.AeroworksCreativeSections
 import de.teutonstudio.ccaeroworks.client.display.DeskDisplayModels
 import de.teutonstudio.ccaeroworks.client.display.RadarOverlayRenderer
 import de.teutonstudio.ccaeroworks.client.ponder.CCAeroworksPonderPlugin
+import de.teutonstudio.ccaeroworks.compat.createradar.RadarTrace
 import de.teutonstudio.ccaeroworks.input.CombinedInputSource
 import de.teutonstudio.ccaeroworks.input.CombinedLeverController
 import de.teutonstudio.ccaeroworks.registry.CCBlockEntities
@@ -20,19 +21,33 @@ import net.neoforged.neoforge.common.NeoForge
 
 object CCAeroworksClient {
     fun register(modBus: IEventBus) {
+        RadarTrace.event(
+            "C00_CLIENT_REGISTER",
+            null,
+            null,
+            "CCAeroworksClient.register entered; registering RadarOverlayRenderer::renderLevel on NeoForge.EVENT_BUS"
+        )
         DeskDisplayModels.init()
         modBus.addListener(::clientSetup)
         modBus.addListener(::registerRenderers)
         modBus.addListener<ModelEvent.RegisterAdditional>(ConsoleMultiblockModels::registerAdditional)
         modBus.addListener<ModelEvent.ModifyBakingResult>(::modifyBakingResult)
         NeoForge.EVENT_BUS.addListener(RadarOverlayRenderer::renderLevel)
+        RadarTrace.event(
+            "C00_OVERLAY_LISTENER_REGISTERED",
+            null,
+            null,
+            "NeoForge.EVENT_BUS.addListener(RadarOverlayRenderer::renderLevel) completed"
+        )
         NeoForge.EVENT_BUS.register(CombinedLeverController)
         NeoForge.EVENT_BUS.register(AeroworksCreativeSections)
         NeoForge.EVENT_BUS.register(GuideBookClientHandler)
     }
 
     private fun clientSetup(event: FMLClientSetupEvent) {
+        RadarTrace.event("C01_CLIENT_SETUP_EVENT", null, null, "FMLClientSetupEvent received; enqueueing client initialization")
         event.enqueueWork {
+            RadarTrace.event("C02_CLIENT_SETUP_WORK", null, null, "client setup work executing")
             ModulePartials.init()
             InputSource.displayName(CombinedInputSource.ID)
             PonderIndex.addPlugin(CCAeroworksPonderPlugin())
@@ -47,10 +62,17 @@ object CCAeroworksClient {
                 }
                 .skipVanillaRender { true }
                 .apply()
+            RadarTrace.event(
+                "C03_COMPUTER_VISUAL_REGISTERED",
+                null,
+                null,
+                "ComputerControlDesk ConsoleVisual registered; RadarDisplay remains in shared overlay"
+            )
         }
     }
 
     private fun registerRenderers(event: EntityRenderersEvent.RegisterRenderers) {
+        RadarTrace.event("C04_REGISTER_RENDERERS", null, null, "registering ComputerControlDeskRenderer")
         event.registerBlockEntityRenderer(
             CCBlockEntities.COMPUTER_CONTROL_DESK.get(),
             ::ComputerControlDeskRenderer
