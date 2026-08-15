0ce4cdfa52f3c65e21acc333093b05df1d551604
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-15 11:19:39 +0200

    Move computer page state outside mixin package

---
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/client/ComputerDeskPage.kt
@@ -0,0 +1,7 @@
+package de.teutonstudio.ccaeroworks.client
+
+/** Work areas embedded into CC:Tweaked's native ComputerControlDesk screen. */
+internal enum class ComputerDeskPage {
+    TERMINAL,
+    CHANNELS
+}
