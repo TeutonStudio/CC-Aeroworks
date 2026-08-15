a4a308164e40daf15c6cf7966f69e5c8496608c0
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-15 12:42:53 +0200

    Register shared reactive UI runtime

--- src/main/kotlin/de/teutonstudio/ccaeroworks/computer/ComputerConsoleAccess.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/computer/ComputerConsoleAccess.kt
@@ -2,15 +2,22 @@ package de.teutonstudio.ccaeroworks.computer
 
 import dan200.computercraft.api.ComputerCraftAPI
 import dan200.computercraft.api.component.ComputerComponent
+import dan200.computercraft.api.lua.IComputerSystem
 import de.teutonstudio.ccaeroworks.CCAeroworks
+import de.teutonstudio.ccaeroworks.computer.reactive.ReactiveDependencyRuntime
 import de.teutonstudio.ccaeroworks.telemetry.TelemetryLuaApi
 import java.lang.ref.WeakReference
 
 class ComputerConsoleAccess(owner: ComputerControlDeskBlockEntity) {
     private val owner = WeakReference(owner)
+    private var reactiveRuntime: ReactiveDependencyRuntime? = null
 
     fun owner(): ComputerControlDeskBlockEntity? =
         owner.get()?.takeIf { !it.isRemoved && it.level != null }
+
+    @Synchronized
+    fun reactive(system: IComputerSystem): ReactiveDependencyRuntime =
+        reactiveRuntime ?: ReactiveDependencyRuntime(system).also { reactiveRuntime = it }
 }
 
 object CCComputerComponents {
@@ -43,6 +50,11 @@ object CCLuaApis {
         }
         ComputerCraftAPI.registerAPIFactory { system ->
             system.getComponent(CCComputerComponents.CONSOLE)?.let(::ComputerWireAdminLuaApi)
+        }
+        ComputerCraftAPI.registerAPIFactory { system ->
+            system.getComponent(CCComputerComponents.CONSOLE)?.let { access ->
+                DisplayUiLuaApi(access, system)
+            }
         }
         registered = true
     }
