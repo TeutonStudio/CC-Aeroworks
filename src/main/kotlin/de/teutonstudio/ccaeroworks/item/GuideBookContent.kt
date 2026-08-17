1d8402665aa5bc96879a95267eba4e25e310263a
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-17 19:51:21 +0200

    Gate fallback radar page behind Create Radars

--- src/main/kotlin/de/teutonstudio/ccaeroworks/item/GuideBookContent.kt
+++ src/main/kotlin/de/teutonstudio/ccaeroworks/item/GuideBookContent.kt
@@ -3,13 +3,21 @@ package de.teutonstudio.ccaeroworks.item
 import net.minecraft.network.chat.Component
 import net.minecraft.server.network.Filterable
 import net.minecraft.world.item.component.WrittenBookContent
+import net.neoforged.fml.ModList
 
 object GuideBookContent {
-    fun create(): WrittenBookContent = WrittenBookContent(
-        Filterable.passThrough("CC-Aeroworks API"),
-        "TeutonStudio",
-        0,
-        (1..8).map { page -> Filterable.passThrough(Component.translatable("book.cc_aeroworks.page_$page")) },
-        true
-    )
+    fun create(): WrittenBookContent {
+        val pages = buildList {
+            addAll(1..6)
+            if (ModList.get().isLoaded("create_radar")) add(7)
+            add(8)
+        }
+        return WrittenBookContent(
+            Filterable.passThrough("CC-Aeroworks Manual / API"),
+            "TeutonStudio",
+            0,
+            pages.map { page -> Filterable.passThrough(Component.translatable("book.cc_aeroworks.page_$page")) },
+            true
+        )
+    }
 }
